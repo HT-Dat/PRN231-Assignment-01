ea734fcf22b6e9b408628b8fffd3c78e5ace1ffb
HT-Dat <hotiendat661@gmail.com>
2022-05-17 17:02:21 +0700

    apply singleton pattern

--- DataAccess/DAO/MemberDAO.cs
+++ DataAccess/DAO/MemberDAO.cs
@@ -2,5 +2,26 @@ namespace DataAccess.DAO;
 
 internal class MemberDAO
 {
+    private static MemberDAO instance = null;
+    private static readonly object instanceLock = new object();
+
+    private MemberDAO()
+    {
+    }
+
+    public static MemberDAO Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                {
+                    instance = new MemberDAO();
+                }
+                return instance;
+            }
+        }
+    }
     
 }
