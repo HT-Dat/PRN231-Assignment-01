fb385ffaac414d890bebf4630c450467ed00eba6
HT-Dat <hotiendat661@gmail.com>
2022-05-18 02:21:21 +0700

    Add repository for Product

--- DataAccess/Repository/IProductRepository.cs
+++ DataAccess/Repository/IProductRepository.cs
@@ -1,6 +1,11 @@
+using BusinessObject;
+
 namespace DataAccess.Repository;
 
 public interface IProductRepository
 {
-    
+    Task<IEnumerable<Product>> GetAll();
+    Task<Product?> Get(int id);
+    Task Delete(int id);
+    Task Update(Product product);
 }
--- DataAccess/Repository/ProductRepository.cs
+++ DataAccess/Repository/ProductRepository.cs
@@ -1,6 +1,27 @@
+using BusinessObject;
+using DataAccess.DAO;
+
 namespace DataAccess.Repository;
 
-public class ProductRepository
+public class ProductRepository : IProductRepository
 {
-    
+    public Task<IEnumerable<Product>> GetAll()
+    {
+        return ProductDAO.Instance.GetAll();
+    }
+
+    public Task<Product?> Get(int id)
+    {
+        return ProductDAO.Instance.Get(id);
+    }
+
+    public Task Delete(int id)
+    {
+        return ProductDAO.Instance.Delete(id);
+    }
+
+    public Task Update(Product product)
+    {
+        return ProductDAO.Instance.Update(product);
+    }
 }
