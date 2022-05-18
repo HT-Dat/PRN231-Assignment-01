21f6990d5e0b2471b93e5f5b6ce0755479ba8819
HT-Dat <hotiendat661@gmail.com>
2022-05-18 22:42:54 +0700

    Apply repository pattern for OrderDetail DAO

--- DataAccess/Repository/IOrderDetailRepository.cs
+++ DataAccess/Repository/IOrderDetailRepository.cs
@@ -1,6 +1,11 @@
+using BusinessObject;
+
 namespace DataAccess.Repository;
 
 public interface IOrderDetailRepository
 {
-    
+    Task<IEnumerable<OrderDetail>> GetAll();
+    Task<OrderDetail?> Get(int productId, int orderId);
+    Task Delete(int productId, int orderId);
+    Task Update(OrderDetail orderDetail);
 }
--- DataAccess/Repository/OrderDetailRepository.cs
+++ DataAccess/Repository/OrderDetailRepository.cs
@@ -1,6 +1,27 @@
+using BusinessObject;
+using DataAccess.DAO;
+
 namespace DataAccess.Repository;
 
-public class OrderDetailRepository
+public class OrderDetailRepository : IOrderDetailRepository
 {
-    
+    public Task<IEnumerable<OrderDetail>> GetAll()
+    {
+        return OrderDetailDAO.Instance.GetAll();
+    }
+
+    public Task<OrderDetail?> Get(int productId, int orderId)
+    {
+        return OrderDetailDAO.Instance.Get(productId, orderId);
+    }
+
+    public Task Delete(int productId, int orderId)
+    {
+        return OrderDetailDAO.Instance.Delete(productId, orderId);
+    }
+
+    public Task Update(OrderDetail orderDetail)
+    {
+        return OrderDetailDAO.Instance.Update(orderDetail);
+    }
 }
