7acd5acdd14d5c9071abd7c2ae4a9a89bb2da1c6
HT-Dat <hotiendat661@gmail.com>
2022-05-19 13:26:07 +0700

    Dependencies injecting

--- eStoreAPI/Program.cs
+++ eStoreAPI/Program.cs
@@ -1,8 +1,17 @@
+using DataAccess.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
