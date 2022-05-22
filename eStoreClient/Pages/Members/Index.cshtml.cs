231c7ea84d74318b203c9cfb58d39801bcf05277
HT-Dat <hotiendat661@gmail.com>
2022-05-23 01:58:03 +0700

    Prevent unauthorized access

--- eStoreClient/Pages/Members/Index.cshtml.cs
+++ eStoreClient/Pages/Members/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using BusinessObject;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace eStoreClient.Pages.Members
@@ -12,8 +13,20 @@ namespace eStoreClient.Pages.Members
 
         public IList<Member> Member { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("LoggedInUser")))
+            {
+                return Unauthorized();
+            }
+
+            Member loggedMember = JsonSerializer.Deserialize<Member>(HttpContext.Session.GetString("LoggedInUser"));
+            if (loggedMember.isAdmin == false)
+            {
+                return Unauthorized();
+            }
+
+
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync("http://localhost:5000/api/Member");
             HttpContent content = response.Content;
@@ -22,6 +35,7 @@ namespace eStoreClient.Pages.Members
                 PropertyNameCaseInsensitive = true
             };
             Member = await JsonSerializer.DeserializeAsync<List<Member>>(content.ReadAsStream(), options);
+            return Page();
         }
     }
 }
